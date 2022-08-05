431385b9bc7f0b8b7cf2e5d0244e95b090df8265
de-sipher <0mu5sfm4@anonaddy.me>
2022-08-05 21:08:14 +0530

    Some temporary inline styles added

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -2,19 +2,25 @@ import React from "react";
 import Card from "./Card";
 
 
-export default function CardGrid({itemList, ...props}){
-    return(
-        <section className="card-grid">
-        {
-            itemList.map((item, i) => <Card 
-                                       
-                                        key={i}
-                                        item={item}
-                                        className='products__card--grid'
-                                     
+export default function CardGrid({ itemList, ...props }) {
+    return (
+        // Move the inline style to a file or global.
+        <section style={{display: 'grid', 
+                        gridTemplateColumns: `repeat(auto-fill, minmax(100px, 200px))`, 
+                        gap: '10px', 
+                        justifyContent: 'space-between', 
+                        alignContent: 'center'
+                    }}>
+            {
+                itemList.map((item, key) => <Card
 
-                                    />)
-        }
+                    key={key}
+                    item={item}
+                    className='products__card--grid'
+
+
+                />)
+            }
         </section>
     )
 }
