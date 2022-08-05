61f9419251bd371284465a362d186bbd080ad086
de-sipher <0mu5sfm4@anonaddy.me>
2022-08-05 21:08:25 +0530

    modified:   components/Card.js 	new file:   components/sidebar.js

--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,27 @@
 import React from "react";
 import Image from 'next/image'
+import styles from "../styles/card.module.css";
 
 
 
-export default function Card({item, className}){
+export default function Card({ item, className, key }) {
 
-  const {id, thumbnail, title, price, rating} = item
+    const { thumbnail, title, price, rating, description } = item
 
-    return(
-        <section className={className}>
-            <article id={id} className={`${className}--img`}>
-                <Image src={thumbnail}
-                         width={228}
-                         height={177}
-                    />
+    return (
+        <section key={key} className={styles.card__container}>
+            {/* <article className={styles.card__img_container}> */}
+            <article className={styles.card__img_container}>
+                <Image src='https://picsum.photos/200/180'
+                    width={200}
+                    height={177}
+                />
             </article>
-            <article id={id} className={`${className}--content`}>
-                <h3>{title}</h3>
-                <li>{price}</li>
-                <li>{rating}</li>
-                <button>Add to card</button>
+            <article className={styles.card__body} >
+                <h3 className={styles.card__body__title}>{title}</h3>
+                <p className={styles.card__body__description}>{description}</p>
+                <p>{price}</p>
+                <button className={styles.card__btn}>Add to card</button>
             </article>
         </section>
     )
