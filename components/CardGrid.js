390b1f855a3cf372a265f6325228789c98a86241
de-sipher <0mu5sfm4@anonaddy.me>
2022-08-09 12:46:55 +0530

    sidebar added. | bg from cardGrid removed

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -2,10 +2,10 @@ import React from "react";
 import Card from "./Card";
 import styles from '../styles/cardGrid.module.css'
 
-export default function CardGrid({ itemList, ...props }) {
+export default function CardGrid({ itemList, style, ...props }) {
 // itemList is an array
     return (
-        <section className={styles.card_container}>
+        <section className={styles.card_container} style={style}>
             {
                 itemList &&
                 itemList.map((item, i) => <Card
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import CardGrid from "../../components/CardGrid";
-
+import Sidebar from "../../components/Sidebar";
 export async function getStaticProps(){
-    const res = await fetch('https://dummyjson.com/products?limit=1')
+    const res = await fetch('https://dummyjson.com/products?limit=10')
     const props = await res.json()
     return {
         props
@@ -12,7 +12,10 @@ export async function getStaticProps(){
 export default function Products({products ,...props}){
     return (
         <>
-            <CardGrid itemList={products} />
+        <section style={{display:'flex'}}>
+            <Sidebar/>
+            <CardGrid itemList={products} style={{flexBasis: '100%'}}/>
+        </section>
         </>
     )
 }
