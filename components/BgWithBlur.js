649e6a1ad44d1d73052e2c962d1ef1b2bf6c2a51
de-sipher <0mu5sfm4@anonaddy.me>
2022-08-14 11:32:07 +0530

    footer done | styling of footer also done | bgWithBlur.js got a new prop called imgBlur which sets the blur amount if specified.

--- components/BgWithBlur.js
+++ components/BgWithBlur.js
@@ -3,43 +3,63 @@ import Image from "next/image";
 
 
 
-export default function BgWithBlur({imgPos ,nobg, bg, height, width, imgContainerStyle, ...props }) {
+export default function BgWithBlur({ imgPos, imgBlur, nobg, bg, height, width, imgContainerStyle, ...props }) {
     let fixedStyle = {
         position: 'absolute',
         overflow: 'hidden',
         height: '100px',
         width: '100px',
-        zIndex: '-1'
+        zIndex: '-1',
+        backdropFilter: 'blur(11.8px)'
     }
+    let glassmorphHigh = {
+        /* From https://css.glass */
+        background: 'rgba(255, 255, 255, 0.01)',
+        backdropFilter: 'blur(11.8px)',
+        // -webkit-backdrop-filter: blur(11.8px),
+    }
+
+    let glassmorphLow = {
+        /* From https://css.glass */
+        background: 'rgba(255, 255, 255, 0.006)',
+        boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+        backdropFilter: 'blur(4px)',
+        // -webkit-backdrop-filter: blur(4px),
+    }
+
     if (height)
         fixedStyle.height = height
     if (width)
         fixedStyle.width = width
+    if (imgBlur){
+        fixedStyle.backdropFilter = `blur(${imgBlur})` 
+    }
 
     return (
         <>
             {/* Background Image */}
             {
                 nobg ?
-                <article className={imgContainerStyle}
-                    style={fixedStyle}
-                >
-                </article>
-                :
-                <article className={imgContainerStyle}
-                    style={fixedStyle}
-                >
-                    <Image src={bg}
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition={imgPos ? imgPos : 'center'}
-                        priority
-                    />
-                </article>
+                    <article className={imgContainerStyle}
+                        style={fixedStyle}
+                    >
+                    </article>
+                    :
+                    <article className={imgContainerStyle}
+                        style={fixedStyle}
+                    >
+                        <Image src={bg}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition={imgPos ? imgPos : 'center'}
+                            priority
+                        />
+                    </article>
             }
             {/* The following article tag is responsible for the frosted glass effect on top of the bg  */}
-            <article className={`${imgContainerStyle} glassmorphHigh`}
-                style={fixedStyle}
+            <article className={`${imgContainerStyle}`}
+                style={fixedStyle
+                }
             >
             </article>
         </>
--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/footer.module.css";
 import Link from "next/link";
-import TextField from '@mui/material/TextField';
-import {Button, Typography} from '@mui/material/';
+import { Button, Typography, TextField, FormControl, InputLabel, Input, FormHelperText } from '@mui/material/';
+import psychedelic from "../public/assets/psychedelic.jpg"
+import BgWithBlur from "./BgWithBlur";
+import { BsFacebook, BsInstagram, BsTwitter, BsTwitch } from "react-icons/bs"
 
+export default function Footer() {
+    useEffect(() => {
+        window.scrollTo(0, 20000)
+    }, [])
+    return (
+        <>
+            <div style={{ position: 'relative' }}>
+                <BgWithBlur bg={psychedelic} height='100%' width='100%' imgPos='center' imgBlur={'4px'} />
+                <section className={styles.footer_container}>
+                    <article className={styles.article_container}>
+                        <Typography variant='h4'>Newsletter</Typography>
+                        <form action="/">
+                            <input className={styles.form_input} placeholder='Enter your email here' type="text"></input>
 
-export default function Footer(){
-    return(
-        <section className={styles.footer_container}>
-            <h2>FOOTER COMPONENT HERE</h2>
-            <TextField text='a'/>
-            <Button variant="text">Submit</Button>
-        </section>
+                            <Button className={styles.form_btn}
+                                type='submit'
+                                variant="contained"
+                                sx={{ borderRightRadius: '100px' }}
+                            >Join
+                            </Button>
+
+                        </form>
+                        <Typography variant='p'>sign up for exclusive offers skin care tips, eco-friendly ideas and get 20% off your 1st purchase!</Typography>
+                    </article>
+                    <article className={styles.article_container}>
+                        <Typography variant='h4'>Follow us</Typography>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', cursor: 'pointer' }}>
+                            <Link href={'#'}><BsFacebook /></Link>
+                            <Link href={'#'}><BsInstagram /></Link>
+                            <Link href={'#'}><BsTwitter /></Link>
+                            <Link href={'#'}><BsTwitch /></Link>
+                        </div>
+                    </article>
+                </section>
+                <section className={styles.copyright_container}>
+                    <Typography variant='p'>copyright and stuff. Wirklich.</Typography>
+                </section>
+            </div>
+        </>
     )
 }
